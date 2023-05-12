0d04c4e37b0562db8e8df1c53bbd72cc63efe54e
Brigulinoo07 <paulo.silva1449@etec.sp.gov.br>
2023-05-12 15:23:14 -0300

    spring

---
+++ src/main/java/br/com/eteczl/workshopspring/homeController.java
@@ -0,0 +1,23 @@
+package br.com.eteczl.workshopspring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class homeController {
+@GetMapping("/")
+    public ModelAndView acessoHome(){
+
+    ModelAndView modelAndView = new ModelAndView();
+    modelAndView.setViewName("home");
+
+    Aluno aluno = new Aluno("Paulo Enrick", "santoss@gmail.com");
+
+    modelAndView.addObject("primeiroAluno",aluno);
+
+    return modelAndView;
+
+}
+
+}
